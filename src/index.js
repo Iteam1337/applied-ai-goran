6c5183f662978f02005f20599bb8cb412b09475e
Sebastian Andréasson <sebastian.andreasson93@gmail.com>
2019-02-08 15:22:44 +0100

    fix stuff

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 const io = require('socket.io')(1337)
 const { spawn } = require('child_process')
-const understand = require('./understand')
 
 const start = () => {
-  console.log('start listen session')
   const listen = spawn('npm', ['run', 'listen'])
 
   listen.stdout.on('data', async data => {
     const text = data.toString()
+    // io.emit here => !
     console.log(text)
-    const sentiment = await understand(text)
-    console.log(sentiment)
   })
 
   listen.on('close', code => {
